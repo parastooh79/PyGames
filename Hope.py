8aec46cf35fb84003fb0c4b68b3354bb0a31a8ef
JIM <47224583+JimByte@users.noreply.github.com>
2020-08-01 13:05:09 +0430

    Update Hope.py

--- Hope.py
+++ Hope.py
@@ -1,13 +1,21 @@
-#EXPORT
-#2
-#4
-#Hope
 c = 0
-for i in range(1,250):
-    if i % 2 == 0:
-        if c < 2 :
-            print(i)
-            c += 1
-        if c==2:
-            print('Hope')
-            c = 0
+inp = int(input("Enter range number : "))
+EvenOdd = input("Even or Odd ? (use E = e or D = d) : ")
+HopeJump=int(input('Enter Hope Jump number :'))
+for i in range(1,inp):
+    if EvenOdd == 'E' or EvenOdd == 'e':
+        if i % 2 == 0:
+            if c < HopeJump:
+                print(i)
+                c += 1
+            if c == HopeJump:
+                print('Hope')
+                c = 0
+    if EvenOdd == 'D' or EvenOdd == 'd':
+        if i % 2 == 1:
+            if c < HopeJump:
+                print(i)
+                c += 1
+            if c == HopeJump:
+                print('Hope')
+                c = 0
