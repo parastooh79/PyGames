537753e025e2be3b9abed6d6ff250891a3c98cdc
JIM <47224583+JimByte@users.noreply.github.com>
2020-08-01 13:32:51 +0430

    Create Start.py

---
+++ Start.py
@@ -0,0 +1,7 @@
+import os
+print('--------------> JimByte Make it <--------------')
+print("GAMES : ")
+print(os.system("ls data"))
+print('-----------------------------------------------')
+inp=input("choos the game File : ")
+response = os.system("python3 data/%s"%inp)
