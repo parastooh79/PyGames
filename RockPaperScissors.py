722f84ea6ad2272fa5f46b516e94554e5d7a396c
JIM <47224583+JimByte@users.noreply.github.com>
2020-08-02 23:43:36 +0430

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -2,20 +2,20 @@ from random import randint
 CoputerPoint = 0
 UserPoint = 0
 for  i in range(10):
-    inp = input('ENTER S K G :')
-    if inp == "S" or inp == "s":
+    inp = input('ENTER Rock Paper Scissors :')
+    if inp == "R" or inp == "r":
         inp = 1
-    if inp == "K" or inp == "k":
+    if inp == "P" or inp == "p":
         inp = 2
-    if inp == "G" or inp == "g":
+    if inp == "K" or inp == "k":
         inp = 3
     ran = randint(1, 3)
     if ran == 1 and inp == 2 or ran == 2 and inp == 3 or ran == 3 and inp == 1:
         UserPoint += 1
-        print("User WIIIN")
+        print("USER WIIIN")
     if ran == 1 and inp == 3 or ran == 2 and inp == 1 or ran == 3 and inp == 2:
         CoputerPoint += 1
-        print("Computer WIIIN")
+        print("COMPUTER WIIIN")
     if ran == 1 and inp == 1 or ran == 2 and inp == 2 or ran == 3 and inp == 3:
-        print("NO ONE WIIIN")
+        print("NO ONE WIN")
 print("COMPUTER POINT :"+str(CoputerPoint)+" | "+"USER POINT :"+str(UserPoint))
